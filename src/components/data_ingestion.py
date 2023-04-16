7990c9c85cf7b8edd495d6fa0aa10eb1168145f3
Kavya Baltha <kavyabaltha@MacBook-Air-9.local>
2023-04-16 17:30:50 -0400

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,12 +3,14 @@ import sys
 cwd = os.getcwd()
 print(cwd)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+print("/n",sys.path)
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -46,5 +48,8 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
 # test
